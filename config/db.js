31bd08d27c67fb13d503c2528bedd50309923143
Priyanshu Ben <priyanshuben42@gmail.com>
2026-08-15 16:40:56 +0530

    deploy backend

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
